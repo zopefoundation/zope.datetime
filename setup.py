340d62bd7f0af3e7ccae35b180d3f99bcdc2116e
Gediminas Paulauskas <menesis@pov.lt>
2011-11-29 16:28:24 +0000

    Preparing release 3.4.1

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name='zope.datetime',
-      version = '3.4.1dev',
+      version='3.4.1',
       url='http://pypi.python.org/pypi/zope.datetime',
       license='ZPL 2.1',
       description='Zope datetime',
