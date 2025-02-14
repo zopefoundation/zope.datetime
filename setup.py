1914be4d61068175616dbd246c9dd0489315cc31
Michael Howitz <icemac@gmx.net>
2025-02-14 11:31:36 +0100

    Preparing release 5.1

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ TESTS_REQUIRE = [
 ]
 
 setup(name='zope.datetime',
-      version='5.1.dev0',
+      version='5.1',
       url='http://github.com/zopefoundation/zope.datetime',
       license='ZPL-2.1',
       description='Zope datetime',
